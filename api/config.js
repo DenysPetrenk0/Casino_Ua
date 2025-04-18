b207740594924fef5b749f6a8b134634771ea1ec
Denys Petrenko <81390655+DenysPetrenk0@users.noreply.github.com>
2025-04-18 09:43:56 +0300

    Update config.js

--- api/config.js
+++ api/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
-const ALLOWED_ORIGIN = process.env.ALLOWED_ORIGIN === 'http://localhost:5000';
+const ALLOWED_ORIGIN = process.env.ALLOWED_ORIGIN;
 
 module.exports = {
 	PORT,
